87086ff91929356909f83f44f72bb6ad18869dbc
Artem0405 <102509928+Artem0405@users.noreply.github.com>
2023-11-16 09:31:33 +0300

    Add files via upload

---
+++ pract22/src/VIC.java
@@ -0,0 +1,12 @@
+public class VIC implements Chair {
+
+    @Override
+    public void sit(Chair chair) {
+        System.out.println("Это VIC");
+    }
+
+    @Override
+    public String GetType() {
+        return "vic";
+    }
+}
